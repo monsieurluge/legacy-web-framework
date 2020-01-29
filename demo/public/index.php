96db73a180bdd2527b4d2bbaade5a6bda3716a52
monsieurluge <monsieurluge@gmail.com>
2020-01-29 23:12:38 +0100

    minor change

--- demo/public/index.php
+++ demo/public/index.php
@@ -6,11 +6,15 @@ use monsieurluge\lwf\Service\BasicServiceProvider;
 use monsieurluge\lwf\Service\PopulatedServiceProvider;
 use monsieurluge\lwfdemo\Config\DummyServices;
 
+// create the service provider
+
 $serviceProvider = new PopulatedServiceProvider(
     new BasicServiceProvider(),
     [
         new DummyServices(),
     ]
 );
+
+// run the application
 
 $serviceProvider->named('dieded')->run();
