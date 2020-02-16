47ff0fbc77225ca739366b1d1ca008696315bcbf
monsieurluge <monsieurluge@gmail.com>
2020-02-16 22:24:51 +0100

    create and use an abstract routes object

--- demo/config/Routing/PagesGet.php
+++ demo/config/Routing/PagesGet.php
@@ -3,24 +3,16 @@
 namespace monsieurluge\lwfdemo\Config\Routing;
 
 use monsieurluge\lwf\Routing\AlwaysHandleRoute;
+use monsieurluge\lwf\Routing\AbstractRoutes;
 use monsieurluge\lwf\Routing\Router;
 use monsieurluge\lwf\Routing\Routes;
 
-final class PagesGet implements Routes
+final class PagesGet extends AbstractRoutes implements Routes
 {
     /**
      * @inheritDoc
      */
-    public function declareTo(Router $router): void
-    {
-        $routes = $this->routes();
-
-        foreach ($routes as $route) {
-            $router->register($route);
-        }
-    }
-
-    private function routes(): array
+    protected function routes(): array
     {
         return [
             new AlwaysHandleRoute()
