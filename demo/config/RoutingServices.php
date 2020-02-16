b35f0ce2db21d41a660ccb3a31d09ac2e717e138
monsieurluge <monsieurluge@gmail.com>
2020-02-16 22:08:46 +0100

    move the route registering logic to a dedicated object

--- demo/config/RoutingServices.php
+++ demo/config/RoutingServices.php
@@ -2,11 +2,12 @@
 
 namespace monsieurluge\lwfdemo\Config;
 
-use monsieurluge\lwf\Routing\AlwaysHandleRoute;
 use monsieurluge\lwf\Routing\BasicRouter;
+use monsieurluge\lwf\Routing\PopulatedRouter;
 use monsieurluge\lwf\Routing\Router;
 use monsieurluge\lwf\Service\ServiceProvider;
 use monsieurluge\lwf\Service\Services;
+use monsieurluge\lwfdemo\Config\Routing\PagesGet;
 
 final class RoutingServices implements Services
 {
@@ -26,11 +27,10 @@ final class RoutingServices implements Services
     {
         return [
             Router::class => function () {
-                $router = new BasicRouter();
-
-                $router->register(new AlwaysHandleRoute());
-
-                return $router;
+                return new PopulatedRouter(
+                    new BasicRouter(),
+                    new PagesGet()
+                );
             }
         ];
     }
